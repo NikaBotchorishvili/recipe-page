27670cbcb4a6437e16098f32a009e929d735d564
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-02-14 20:45:08 +0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["index.html"],
+	theme: {
+		colors: {
+            // Primary
+
+			secondary: "hsl(14, 45%, 36%)",
+			main: "hsl(332, 51%, 32%)",
+
+            // Neutral
+
+		    white: "hsl(0, 0%, 100%)",
+			roseWhite: "hsl(330, 100%, 98%)",
+			Eggshell: "hsl(30, 54%, 90%)",
+			lightGrey: "hsl(30, 18%, 87%)",
+			wengeBrown: "hsl(30, 10%, 34%)",
+			darkCharcoal: "hsl(24, 5%, 18%)",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
