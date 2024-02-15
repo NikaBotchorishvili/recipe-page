5262f250cfab4e8c8e041fd9fb54218b03544e88
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-02-15 13:05:27 +0400

    Style: Finished up styling the whole layout responsively

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,23 @@ module.exports = {
 	content: ["index.html"],
 	theme: {
 		colors: {
-            // Primary
+			// Primary
 
 			secondary: "hsl(14, 45%, 36%)",
 			main: "hsl(332, 51%, 32%)",
 
-            // Neutral
+			// Neutral
 
-		    white: "hsl(0, 0%, 100%)",
+			white: "hsl(0, 0%, 100%)",
 			roseWhite: "hsl(330, 100%, 98%)",
 			Eggshell: "hsl(30, 54%, 90%)",
 			lightGrey: "hsl(30, 18%, 87%)",
 			wengeBrown: "hsl(30, 10%, 34%)",
 			darkCharcoal: "hsl(24, 5%, 18%)",
+		},
+		fontFamily: {
+			outfit: "'Outfit', sans-serif",
+			young: "'Young Serif', serif",
 		},
 		extend: {},
 	},
